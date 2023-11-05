f3f304c6a0cc7530fbb80917f6287bfb1fcb9dee
saj62 <sarannjac@gmail.com>
2023-11-05 04:14:17 -0600

    worked on welcome page

---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './welcomepage.css';
+
+const WelcomePage = () => {
+    return (
+        <div className="welcomepage">
+
+            <div className="navbar">
+                <div className="menu">
+                    <div className="home-text">
+                        <h1>Helping Small Businesses Grow</h1>
+                        <p>Quote</p>
+
+                        <div className="home-btn">Log In</div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default WelcomePage;
